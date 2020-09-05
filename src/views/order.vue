d6eb5de4b08a086be8c80bd6f6b77124fca0f393
sym <yum.shen@htd.cn>
2020-09-05 19:27:15 +0800

    order

--- src/views/order.vue
+++ src/views/order.vue
@@ -1,10 +1,45 @@
 <template>
     <div>
         <cloud-component></cloud-component>
-        <div class="loginWrap">
-            <img class="logo" src="../assets/image/logo.png" alt="">
-            <img class="welcome" src="../assets/image/welcome.png" alt="">
-            <img class="discord" @click="login" src="../assets/image/discord.png" alt="">
+        <head-component></head-component>
+        <div class="orderWrap">
+            <div class="leftInfo">
+                <div class="typeWrap">
+                    <div class="title">TYPE</div>
+                    <div class="type" v-for="item in chooseType" :key="item.name">
+                        <p class="typeLi">
+                            {{item.name}}
+                        </p>
+                    </div>
+                </div>
+                <div class="noStatus content">
+                    <img class="noStatus" src="../assets/image/rainNo.png" alt="">
+                </div>
+                <div class="hasStatus content">
+    
+                </div>
+            </div>
+            <div class="rightInfo">
+                <div class="top">
+                    <p class="name">Total</p>
+                    <p class="detail">XXGB</p>
+                    <p class="name">Used</p>
+                    <p class="detail">XXGB</p>
+                    <p class="name">Remain</p>
+                    <p class="detail">XXGB</p>
+                </div>
+                <div class="generate purple">
+                    GENERATE
+                </div>
+                <div class="generate grey">
+                    DELETE
+                </div>
+                <div class="times">
+                    <p class="expire">EXPIRE IN</p>
+                    <p class="days"><span class="hours">23</span>DAYS</p>
+                    <p class="hours">24：00</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -12,109 +47,140 @@
 
 <script>
 import cloudComponent from '@/components/cloudComponent'
+import headComponent from '@/components/headComponent'
 import Utils from "@/utils/util"
-import Oidc from "oidc-client";
-var config = {
-    authority: "http://localhost:5000",
-    client_id: "js",
-    redirect_uri: "http://localhost:5003/CallBack",
-    response_type: "id_token token",
-    scope: "openid profile api1",
-    post_logout_redirect_uri: "http://localhost:5003/"
-};
-var mgr = new Oidc.UserManager(config);
 export default {
-    name: "login",
+    name: "order",
     components: {
-        cloudComponent
+        cloudComponent,
+        headComponent
     },
     data() {
-        return {}
+        return {
+            chooseType: [{
+                name: '2.0'
+            }, {
+                name: '3.0'
+            }],
+            chooseWeb: [{
+                name: 'Footisite'
+            }, {
+                name: 'Shopify'
+            }],
+            chooseRegion: [{
+                name: 'US'
+            }, {
+                name: 'DE'
+            }, {
+                name: 'AU'
+            }, {
+                name: 'UK'
+            }, {
+                name: 'FR'
+            }],
+            selectType: '',
+            selectWeb: '',
+            selectRegion: '',
+            amount: ''
+        }
     },
     methods: {
-        login() {
-            var that = this;
-            mgr.getUser().then(function(user) {
-                var url = "http://localhost:5001/identity";
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", url);
-                xhr.onload = function() {
-                    that.res = (xhr.status, JSON.parse(xhr.responseText))
-                };
-                xhr.setRequestHeader("Authorization", "Bearer " + user.access_token);
-                xhr.send();
-            });
-        },
-        logout() {
-            mgr.signoutRedirect();
-        }
+
     },
     computed: {},
     created() {},
-    mounted() {
-        var that = this;
-        mgr.getUser().then(function(user) {
-            if (user) {
-                // this.res = ("User logged in", user.profile);
-                that.res = ("User logged in", user.profile);
-            } else {
-                that.res = ("User not logged in");
-            }
-        });
-    }
 };
 </script>
 
 <style lang="scss" scoped>
-.loginWrap {
-    width: 280px;
-    padding: 50px 0 50px 0;
-    border-radius: 20px;
-    background-color: #fff;
+.orderWrap {
+    width: 1000px;
     margin: 0 auto;
-    text-align: center;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    .logo {
-        display: block;
-        margin: 0 auto;
-        height: 150px;
-    }
-    .welcome {
-        display: block;
-        margin: 0 auto;
-        margin-bottom: 30px;
-    }
-    .main {
-        .commonInput {
-            background-color: #d3d3d3;
-            border: 1px solid #d3d3d3;
-            line-height: 50px;
-            height: 50px;
-            border-radius: 50px;
-            width: 262px;
-            margin: 0 auto;
-            padding-left: 20px;
+    position: relative;
+    .typeWrap {
+        position: absolute;
+        left: 70px;
+        top: -20px;
+        color: #fff;
+        background-color: #ff496f;
+        text-align: center;
+        line-height: 25px;
+        border-radius: 15px;
+        padding: 10px 20px;
+        .title {
+            // border-bottom: 1px solid #fff;
         }
-        .name {
-            margin-bottom: 25px;
-        }
-        .pwd {
-            margin-bottom: 40px;
+        .type {
+            .typeLi {
+                margin: 5px 0;
+            }
         }
     }
-    .go {
-        display: block;
-        margin: 0 auto;
-        margin-bottom: 10px;
-        cursor: pointer;
+    .leftInfo {
+        float: left;
+        .content {
+            width: 800px;
+        }
+        .noStatus {
+            width: 800px;
+            height: 550px;
+        }
     }
-    .discord {
-        display: block;
-        margin: 0 auto;
-        cursor: pointer;
+    .rightInfo {
+        float: right;
+        margin-right: 80px;
+        margin-top: 20px;
+        .top {
+            padding: 40px 30px;
+            color: #6d6d6e;
+            background-color: rgba(255, 255, 255, 0.7);
+            border-radius: 10px;
+            text-align: center;
+            .name {
+                font-size: 16px;
+                font-weight: bold;
+            }
+            .detail {
+                font-size: 14px;
+                margin-bottom: 15px;
+            }
+        }
+        .generate {
+            height: 40px;
+            line-height: 40px;
+            border-radius: 20px;
+            margin: 20px 0px;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: center;
+            padding: 0 10px;
+            color: #fff;
+        }
+        .purple {
+            background: -webkit-linear-gradient(to right, #ba86e8, #cba5cc);
+            background: -o-linear-gradient(to right, #ba86e8, #cba5cc);
+            background: -moz-linear-gradient(to right, #ba86e8, #cba5cc);
+            background: linear-gradient(to right, #ba86e8, #cba5cc);
+        }
+        .grey {
+            background: -webkit-linear-gradient(to right, #a4a4a2, #cbcbcb);
+            background: -o-linear-gradient(to right, #a4a4a2, #cbcbcb);
+            background: -moz-linear-gradient(to right, #a4a4a2, #cbcbcb);
+            background: linear-gradient(to right, #a4a4a2, #cbcbcb);
+        }
+        .times {
+            text-align: center;
+            color: #6d6d6c;
+            .expire {
+                margin-top: 20px;
+                line-height: 20px;
+            }
+            .hours {
+                color: #5bc9ef;
+                font-size: 16px;
+                font-weight: bold;
+            }
+        }
     }
 }
 </style>
